616333744bf71f2e4e4d5f13f513f9b752d5fef1
Reiner Huechting <dev@reinerhuechting.de>
2024-12-13 13:38:14 +0100

    AreaRectangle gelöst.

--- calculations/forms/area_rectangle.go
+++ calculations/forms/area_rectangle.go
@@ -3,6 +3,5 @@ package forms
 // Erwartet zwei Seitenlängen a und b.
 // Liefert die Fläche des entsprechenden Rechtecks.
 func AreaRectangle(a, b float64) float64 {
-	// TODO
-	return 0
+	return a * b
 }
